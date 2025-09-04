ea1dcee3f3bc6af9794c788fc03cdc4a76c7a5ad
Gromi <hwj20010504@163.com>
2025-09-04 20:28:00 +0800

    first commit

---
+++ Infrastructure/Gromi.Infra.Utils/Helpers/LogHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Gromi.Infra.Utils.Helpers
+{
+    /// <summary>
+    /// 日志帮助类
+    /// </summary>
+    public static class LogHelper
+    {
+    }
+}
---
+++ Presentation/Gromi.Template.Api/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gromi.Template.Api
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
