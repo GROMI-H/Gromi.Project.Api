2971dcebf8e3225c4fbd01546c72d66d9b7a9eec
GROMI <hwj20010504@163.com>
2025-10-30 19:36:29 +0800

    添加Session帮助类，待完善

---
+++ Infrastructure/Gromi.Infra.Utils/Helpers/SessionHelper.cs
@@ -0,0 +1,41 @@
+﻿namespace Gromi.Infra.Utils.Helpers
+{
+    /// <summary>
+    /// 会话帮助类
+    /// </summary>
+    public static class SessionHelper
+    {
+        /// <summary>
+        /// 获取session对象
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static object GetSession(string name)
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// 设置Session对象
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public static void SetSession(string name, object value)
+        {
+        }
+
+        /// <summary>
+        /// 删除指定Session
+        /// </summary>
+        /// <param name="name"></param>
+        public static void RemoveSession(string name)
+        { }
+
+        /// <summary>
+        /// 清除所有的Session
+        /// </summary>
+        public static void ClearSession()
+        {
+        }
+    }
+}
